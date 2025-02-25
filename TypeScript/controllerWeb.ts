440fbdab379525279a5e3e08c940a413e8916b05
amit reuveni <amitr123r@gmail.com>
2025-02-25 19:11:13 +0200

    add first consts in controllers

--- TypeScript/controllerWeb.ts
+++ TypeScript/controllerWeb.ts
@@ -1,1 +1,16 @@
-import { Student, createStudent } from './modelWeb.js'
+import { Student, createStudent } from "./modelWeb.js";
+
+const loginButton = document.getElementById("login-btn");
+loginButton?.addEventListener("click", () => {
+    alert("conected");
+});
+
+const signUpButton = document.getElementById("sign-up-button");
+signUpButton?.addEventListener("click", () => {
+    alert("user created");
+});
+
+const userNameInput = document.getElementById("userName") as HTMLInputElement;
+const passwordInput = document.getElementById("password") as HTMLInputElement;
+
+
---
+++ TypeScript/dist/controllerWeb.js
@@ -0,0 +1,10 @@
+const loginButton = document.getElementById("login-btn");
+loginButton?.addEventListener("click", () => {
+    alert("conected");
+});
+const signUpButton = document.getElementById("sign-up-button");
+signUpButton?.addEventListener("click", () => {
+    alert("user created");
+});
+const userNameInput = document.getElementById("userName");
+const passwordInput = document.getElementById("password");
