05815ed7ee8070fc061c0bbbe2024662af81057b
amit reuveni <amitr123r@gmail.com>
2025-02-25 20:04:09 +0200

    add create user button const and fill it

--- TypeScript/controllerWeb.ts
+++ TypeScript/controllerWeb.ts
@@ -33,3 +33,31 @@ signUpButtonFromLogin?.addEventListener("click", (e: Event) => {
 });
 
 
+const createUserButton = document.getElementById("create-user-button");
+createUserButton?.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+
+    const username = (document.getElementById("username") as HTMLInputElement).value;
+    const password = (document.getElementById("password") as HTMLInputElement).value;
+    const firstName = (document.getElementById("first-name") as HTMLInputElement).value;
+    const lastName = (document.getElementById("last-name") as HTMLInputElement).value;
+    const email = (document.getElementById("email") as HTMLInputElement).value;
+    const phone = (document.getElementById("phone") as HTMLInputElement).value;
+    const birthdate = (document.getElementById("birthdate") as HTMLInputElement).value;
+
+    if (!username || !password || !firstName || !lastName || !email || !phone || !birthdate) {
+    alert("Please fill in all the fields.");
+    return;        
+    }
+
+    const savedUsers: Student[] = JSON.parse(localStorage.getItem(studentStoregedKey) || "[]");
+    if (savedUsers.find(f => f.username === username)) {
+        alert("Username already exists. Please choose diffrent username.");
+    }
+    
+    const newStudent = createStudent(username, password, firstName, lastName, email, phone, birthdate);
+    savedUsers.push(newStudent);
+    localStorage.setItem(studentStoregedKey, JSON.stringify(savedUsers));
+
+    window.location.href = "login.html";
+});
--- TypeScript/dist/controllerWeb.js
+++ TypeScript/dist/controllerWeb.js
@@ -1,3 +1,4 @@
+import { createStudent } from "./modelWeb.js";
 const studentStoregedKey = "students";
 const loginButton = document.getElementById("login-btn");
 loginButton?.addEventListener("click", (e) => {
@@ -21,4 +22,27 @@ const signUpButtonFromLogin = document.getElementById("sign-up-button");
 signUpButtonFromLogin?.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "createUser.html";
+});
+const createUserButton = document.getElementById("create-user-button");
+createUserButton?.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const birthdate = document.getElementById("birthdate").value;
+    if (!username || !password || !firstName || !lastName || !email || !phone || !birthdate) {
+        alert("Please fill in all the fields.");
+        return;
+    }
+    const savedUsers = JSON.parse(localStorage.getItem(studentStoregedKey) || "[]");
+    if (savedUsers.find(f => f.username === username)) {
+        alert("Username already exists. Please choose diffrent username.");
+    }
+    const newStudent = createStudent(username, password, firstName, lastName, email, phone, birthdate);
+    savedUsers.push(newStudent);
+    localStorage.setItem(studentStoregedKey, JSON.stringify(savedUsers));
+    window.location.href = "login.html";
 });
