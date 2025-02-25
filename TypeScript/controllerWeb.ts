4e6bfc9186691e7663423e16e600ad6052a4912f
amit reuveni <amitr123r@gmail.com>
2025-02-25 20:03:40 +0200

    add to controller sign up from login const

--- TypeScript/controllerWeb.ts
+++ TypeScript/controllerWeb.ts
@@ -1,16 +1,35 @@
 import { Student, createStudent } from "./modelWeb.js";
 
+const studentStoregedKey = "students";
+
 const loginButton = document.getElementById("login-btn");
-loginButton?.addEventListener("click", () => {
-    alert("conected");
-});
+loginButton?.addEventListener("click", (e: Event) => {
+    e.preventDefault();
 
-const signUpButton = document.getElementById("sign-up-button");
-signUpButton?.addEventListener("click", () => {
-    alert("user created");
-});
+    const userNameInput = (document.getElementById("UserName") as HTMLInputElement).value;
+    const passwordInput = (document.getElementById("password") as HTMLInputElement).value;
 
-const userNameInput = document.getElementById("userName") as HTMLInputElement;
-const passwordInput = document.getElementById("password") as HTMLInputElement;
+    if (!userNameInput || !passwordInput) {
+        alert("Please enter both user username and password.");
+        return;
+    }
+
+
+    const savedUsers: Student[] = JSON.parse(localStorage.getItem(studentStoregedKey) || '[]');
+    const user = savedUsers.find(f => f.username === userNameInput && f.password === passwordInput);
+
+    if (!user) {
+        alert("Invalid user name or password");
+    } else {
+        window.location.href = "webSchool.html";
+    }
+
+
+});
+const signUpButtonFromLogin = document.getElementById("sign-up-button");
+signUpButtonFromLogin?.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    window.location.href = "createUser.html";
+});
 
 
--- TypeScript/dist/controllerWeb.js
+++ TypeScript/dist/controllerWeb.js
@@ -1,10 +1,24 @@
+const studentStoregedKey = "students";
 const loginButton = document.getElementById("login-btn");
-loginButton?.addEventListener("click", () => {
-    alert("conected");
+loginButton?.addEventListener("click", (e) => {
+    e.preventDefault();
+    const userNameInput = document.getElementById("UserName").value;
+    const passwordInput = document.getElementById("password").value;
+    if (!userNameInput || !passwordInput) {
+        alert("Please enter both user username and password.");
+        return;
+    }
+    const savedUsers = JSON.parse(localStorage.getItem(studentStoregedKey) || '[]');
+    const user = savedUsers.find(f => f.username === userNameInput && f.password === passwordInput);
+    if (!user) {
+        alert("Invalid user name or password");
+    }
+    else {
+        window.location.href = "webSchool.html";
+    }
 });
-const signUpButton = document.getElementById("sign-up-button");
-signUpButton?.addEventListener("click", () => {
-    alert("user created");
+const signUpButtonFromLogin = document.getElementById("sign-up-button");
+signUpButtonFromLogin?.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "createUser.html";
 });
-const userNameInput = document.getElementById("userName");
-const passwordInput = document.getElementById("password");
